8946acb05a4adfbe3a9d2db0e1f0a53d44b376d0
optimalcharb <zcharliebauer@gmail.com>
2025-12-20 16:43:08 -0500

    feat: password hashing

--- src/db-fns/users.ts
+++ src/db-fns/users.ts
@@ -1,13 +1,23 @@
 import { createServerFn } from "@tanstack/react-start"
+import * as bcrypt from "bcrypt"
 import { eq } from "drizzle-orm/sql"
 import { z } from "zod"
 import { db } from "@/db/client"
 import { users } from "@/db/schema/users"
 
+const SALT_ROUNDS = 12
 const MIN_PW_LEN = 8
 
+async function hashPassword(password: string) {
+  return await bcrypt.hash(password, SALT_ROUNDS)
+}
+
+async function verifyPassword(password: string, hash: string) {
+  return await bcrypt.compare(password, hash)
+}
+
 // ** CREATE **
-const CreateUserSchema = z.object({
+export const CreateUserSchema = z.object({
   email: z.email(),
   password: z.string().min(MIN_PW_LEN, `Password must be at least ${MIN_PW_LEN} characters long`),
   firstName: z.string().optional(),
@@ -19,7 +29,8 @@ const CreateUserSchema = z.object({
 export const createUser = createServerFn({ method: "POST" })
   .inputValidator(CreateUserSchema)
   .handler(async ({ data }) => {
-    await db.insert(users).values(data)
+    const hash = await hashPassword(data.password)
+    await db.insert(users).values({ ...data, password: hash })
     return { success: true }
   })
 
@@ -44,9 +55,24 @@ export const getUserByEmail = createServerFn({ method: "GET" })
     return user
   })
 
+export const verifyUserPassword = createServerFn({ method: "POST" })
+  .inputValidator(z.object({ email: z.email(), password: z.string() }))
+  .handler(async ({ data }) => {
+    try {
+      const user = await getUserByEmail({ data: { email: data.email } })
+      const isValid = await verifyPassword(data.password, user.password)
+      return { success: isValid }
+    } catch (_e) {
+      // add a small random delay to prevent timing attacks
+      await new Promise((resolve) => setTimeout(resolve, Math.random() * 10))
+      // return false genericly to prevent username enumeration attacks
+      return { success: false }
+    }
+  })
+
 // ** UPDATE **
-// partial create schema with id still required
-const UpdateUserSchema = CreateUserSchema.partial().extend({
+// partial create schema with id required and no password
+export const UpdateUserSchema = CreateUserSchema.omit({ password: true }).partial().extend({
   id: z.string(),
 })
 
