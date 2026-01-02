70e63d87064013dada88e9545cb5353a12e1c331
optimalcharb <zcharliebauer@gmail.com>
2026-01-01 23:43:08 -0500

    style: put prettier and eslint config in root repo dir to allow formatting from root dir and frontend dir

---
+++ eslint.config.mjs
@@ -0,0 +1,72 @@
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptEslintParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+
+
+export default [
+  {
+    ignores: [
+      "node_modules/**",
+      "storybook-static/**",
+      "dist/**",
+      "build/**",
+      "**/shadcn-ui/**",
+      "**/*.stories.tsx",
+      "src/routes/routeTree.gen.ts",
+    ],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      import: importPlugin,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "@typescript-eslint": typescriptEslintPlugin,
+    },
+    languageOptions: {
+      parser: typescriptEslintParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.flat.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      ...typescriptEslintPlugin.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+      "react/react-in-jsx-scope": "off", // Not needed with new JSX transform
+      "sort-imports": [
+        "error",
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+        },
+      ],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" },
+      ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          bun: true,
+          project: "./tsconfig.json",
+        },
+      },
+    },
+  },
+]
