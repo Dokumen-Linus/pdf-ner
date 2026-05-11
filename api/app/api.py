289d033810ba22df31f97e0de32b8c5af935aa91
optimalcharb <zcharliebauer@gmail.com>
2026-05-10 21:14:36 -0400

    revert(api): remove llm_ner domain

--- api/app/api.py
+++ api/app/api.py
@@ -2,13 +2,11 @@ from fastapi import APIRouter, Depends
 
 from .core.dependencies import verify_api_key
 from .domains.avatar_storage.router import router as avatar_storage_router
-from .domains.llm_ner.router import router as llm_ner_router
 from .domains.pdf_storage.router import router as pdf_storage_router
 from .domains.pdf_utils.router import router as pdf_utils_router
 
 api_router = APIRouter(prefix="/api/v1", dependencies=[Depends(verify_api_key)])
 
-api_router.include_router(llm_ner_router)
 api_router.include_router(pdf_utils_router)
 api_router.include_router(pdf_storage_router)
 api_router.include_router(avatar_storage_router)
