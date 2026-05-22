5af5e4d628cb2c592861d5aa394e594f0608e5b4
optimalcharb <zcharliebauer@gmail.com>
2026-05-21 23:17:56 -0400

    ci: delete old deploy scripts

--- infra/init/aws-setup.d/05_avatars_bucket.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-setup_avatar_bucket() {
-  echo ""
-  echo ">>> Avatar bucket"
-  require_setup_values "Avatar bucket" AVATARS_S3_BUCKET_NAME
-  ensure_bucket "$AVATARS_S3_BUCKET_NAME"
-}
